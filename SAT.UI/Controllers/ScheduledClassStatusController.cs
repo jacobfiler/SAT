f29dc7c77c781e9a2b858a5629d1e5bd35b989f4
jacobfiler <jacobwilliamfiler@gmail.com>
2020-07-23 14:58:26 -0500

    Added controllers and views for all database tables; also secured them

---
+++ SAT.UI/Controllers/ScheduledClassStatusController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SAT.DATA;
+
+namespace SAT.UI.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class ScheduledClassStatusController : Controller
+    {
+        private SATEntities db = new SATEntities();
+
+        // GET: ScheduledClassStatus
+        public ActionResult Index()
+        {
+            return View(db.ScheduledClassStatus.ToList());
+        }
+
+        // GET: ScheduledClassStatus/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ScheduledClassStatu scheduledClassStatu = db.ScheduledClassStatus.Find(id);
+            if (scheduledClassStatu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(scheduledClassStatu);
+        }
+
+        // GET: ScheduledClassStatus/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ScheduledClassStatus/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SCID,SCName,SCDescription")] ScheduledClassStatu scheduledClassStatu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ScheduledClassStatus.Add(scheduledClassStatu);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(scheduledClassStatu);
+        }
+
+        // GET: ScheduledClassStatus/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ScheduledClassStatu scheduledClassStatu = db.ScheduledClassStatus.Find(id);
+            if (scheduledClassStatu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(scheduledClassStatu);
+        }
+
+        // POST: ScheduledClassStatus/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SCID,SCName,SCDescription")] ScheduledClassStatu scheduledClassStatu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(scheduledClassStatu).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(scheduledClassStatu);
+        }
+
+        // GET: ScheduledClassStatus/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ScheduledClassStatu scheduledClassStatu = db.ScheduledClassStatus.Find(id);
+            if (scheduledClassStatu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(scheduledClassStatu);
+        }
+
+        // POST: ScheduledClassStatus/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ScheduledClassStatu scheduledClassStatu = db.ScheduledClassStatus.Find(id);
+            db.ScheduledClassStatus.Remove(scheduledClassStatu);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SAT.UI/Controllers/ScheduledClassesController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SAT.DATA;
+
+namespace SAT.UI.Controllers
+{
+    public class ScheduledClassesController : Controller
+    {
+        private SATEntities db = new SATEntities();
+
+        // GET: ScheduledClasses
+        public ActionResult Index()
+        {
+            var scheduledClasses = db.ScheduledClasses.Include(s => s.Cours).Include(s => s.ScheduledClassStatu);
+            return View(scheduledClasses.ToList());
+        }
+
+        // GET: ScheduledClasses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ScheduledClass scheduledClass = db.ScheduledClasses.Find(id);
+            if (scheduledClass == null)
+            {
+                return HttpNotFound();
+            }
+            return View(scheduledClass);
+        }
+
+        // GET: ScheduledClasses/Create
+        public ActionResult Create()
+        {
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "CourseName");
+            ViewBag.SCID = new SelectList(db.ScheduledClassStatus, "SCID", "SCName");
+            return View();
+        }
+
+        // POST: ScheduledClasses/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ScheduledClassID,CourseID,StartDate,EndDate,InstructorName,Location,SCID")] ScheduledClass scheduledClass)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ScheduledClasses.Add(scheduledClass);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "CourseName", scheduledClass.CourseID);
+            ViewBag.SCID = new SelectList(db.ScheduledClassStatus, "SCID", "SCName", scheduledClass.SCID);
+            return View(scheduledClass);
+        }
+
+        // GET: ScheduledClasses/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ScheduledClass scheduledClass = db.ScheduledClasses.Find(id);
+            if (scheduledClass == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "CourseName", scheduledClass.CourseID);
+            ViewBag.SCID = new SelectList(db.ScheduledClassStatus, "SCID", "SCName", scheduledClass.SCID);
+            return View(scheduledClass);
+        }
+
+        // POST: ScheduledClasses/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ScheduledClassID,CourseID,StartDate,EndDate,InstructorName,Location,SCID")] ScheduledClass scheduledClass)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(scheduledClass).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "CourseName", scheduledClass.CourseID);
+            ViewBag.SCID = new SelectList(db.ScheduledClassStatus, "SCID", "SCName", scheduledClass.SCID);
+            return View(scheduledClass);
+        }
+
+        // GET: ScheduledClasses/Delete/5
+        [Authorize(Roles = "Admin")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ScheduledClass scheduledClass = db.ScheduledClasses.Find(id);
+            if (scheduledClass == null)
+            {
+                return HttpNotFound();
+            }
+            return View(scheduledClass);
+        }
+
+        // POST: ScheduledClasses/Delete/5
+        [Authorize(Roles = "Admin")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ScheduledClass scheduledClass = db.ScheduledClasses.Find(id);
+            db.ScheduledClasses.Remove(scheduledClass);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
